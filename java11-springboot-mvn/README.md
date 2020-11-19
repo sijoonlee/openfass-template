87a1dda5fb0b863757f51a04842d357f40f28305
Sijoon Lee <shijoonlee@gmail.com>
2020-11-19 11:28:29 -0800

    simplified

--- java11-springboot-mvn/README.md
+++ java11-springboot-mvn/README.md
@@ -3,31 +3,7 @@
 - Springframework : 2.3.5.RELEASE
     - spring-boot-starter-web with jetty server
 - Maven : 3.6.3
-- Stick to using com.openfaas.model
-- com.openfaas.endpoint replaced with customized code (ResponseController.java)
-    - See the comparison below
 
-## Comparison
-This is to compare the endpoint.App from openfaas's offical endpoint library
-with the customized code for Springframework
-
-### Notation
-- com.openfaas.springendpoint.ResponseController = Spring.App (under function)
-- com.openfass.entrypoint.App = Openfaas.App (under external lib)
-
-### Comparison Table
-|                |  Openfaas.App    | Spring.App             |
-|---             |  ---      |   ---             |
-|Handler Instance| Singleton Pattern | @Autowire  |
-|Request through | HttpExchange(com.sun.net.httpserver)|HttpServletRequest(javax.servlet.http)|
-|Response through| HttpExchange(com.sun.net.httpserver)|HttpServletResponse(javax.servlet.http)|
-|Step1           | Extract Req from HttpExchange | Extract Req from HttpServletRequest |
-|Step2           | Instantiate IRequest obj(req) | Instantiate IRequest obj(req) |
-|Step3           | Pass req to Handler.handle()  | Pass req to Handler.handle() |
-|                | Instantiate IResponse res     | Instantiate IResponse res |
-|Step4           | Extract info from res         | Extract info from res |
-|Step5           | Send back using HttpExchange  | Send back using HttpServletResponse(javax.servlet.http)|
-  
   
 
 
--- java11-springboot-mvn/function/src/main/java/com/openfaas/function/Handler.java
+++ java11-springboot-mvn/function/src/main/java/com/openfaas/function/Handler.java
@@ -1,38 +1,49 @@
 package com.openfaas.function;
 
-import com.openfaas.model.IRequest;
-import com.openfaas.model.IResponse;
-import com.openfaas.model.Response;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
+//import com.openfaas.model.Request;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-@Component
-public class Handler extends com.openfaas.model.AbstractHandler {
+@Controller
+public class Handler {
+    private static Logger logger = LoggerFactory.getLogger(Handler.class);
 
-    //private static Logger logger = LoggerFactory.getLogger(Handler.class);
+    @RequestMapping(value="/")
+    public @ResponseBody String handle(
+            @RequestParam(required = false) Map<String, String> requestParam,
+            RequestEntity<String> requestEntity)  {
 
-    public IResponse Handle(IRequest req) {
-        Response res = new Response();
-
-        res.setContentType(MediaType.TEXT_PLAIN_VALUE);
-
-        String headers = "";
-
-        for(Map.Entry<String,String> entry : req.getHeaders().entrySet()){
-            headers += ("\n" + entry.getKey() + " - " + entry.getValue());
+        String uri = requestEntity.getUrl().toString();
+        String method = requestEntity.getMethod().toString();
+        Map<String, List<String>> headers = requestEntity.getHeaders();
+        Map<String, String> headersChopped= new HashMap<>();
+        for(Map.Entry<String, List<String>> entry : headers.entrySet()){
+            headersChopped.put(entry.getKey(), entry.getValue().get(0));
         }
+        String params = requestParam.entrySet().toString();
+        String body = requestEntity.getBody();
+        String info =
+                "Req URI: " + uri + "\n" +
+                        "Req Method: " + method + "\n" +
+                        "Req Headers: " + headers + "\n" +
+                        "Req Params: " + params + "\n" +
+                        "Req Body: " + body + "\n";
 
-        res.setBody("Hello, World!"
-                + "\nBody: " + req.getBody()
-                + "\nQuery: " + req.getQueryRaw()
-                + "\nPath: " + req.getPathRaw()
-                + "\nHeaders From Request"
-                + headers);
+        //Request req = new Request(body, headersChopped);
 
-        return res;
+        //String bodyFromReq = req.getBody();
+
+        return info;
     }
 }
