20ef20858eea31b9198645cadf3ea2dc8fcf60a2
Sijoon Lee <shijoonlee@gmail.com>
2020-11-19 13:28:52 -0800

    housekeeping

--- java11-springboot-mvn/function/src/main/java/com/openfaas/function/Handler.java
+++ java11-springboot-mvn/function/src/main/java/com/openfaas/function/Handler.java
@@ -1,22 +1,17 @@
 package com.openfaas.function;
 
-import com.openfaas.model.Request;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.*;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
 public class Handler {
-    private static Logger logger = LoggerFactory.getLogger(Handler.class);
 
     @RequestMapping(value="/")
     public ResponseEntity<Map<String, String>> handle(
@@ -25,18 +20,10 @@ public class Handler {
 
         Map<String, String> responseBody = new HashMap<>();
         responseBody.put("Req URI", requestEntity.getUrl().toString());
-        responseBody.put("Req Method", requestEntity.getHeaders().toString());
+        responseBody.put("Req Headers", requestEntity.getHeaders().toString());
+        responseBody.put("Req Method", requestEntity.getMethod().toString());
         responseBody.put("Req Params", requestParam.toString());
         responseBody.put("Req Body", requestEntity.getBody());
-
-        Map<String, List<String>> headers = requestEntity.getHeaders();
-        Map<String, String> headersChopped= new HashMap<>();
-        for(Map.Entry<String, List<String>> entry : headers.entrySet()){
-            headersChopped.put(entry.getKey(), entry.getValue().get(0));
-        }
-        Request req = new Request(requestEntity.getBody(), headersChopped);
-
-        //String bodyFromReq = req.getBody();
 
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
